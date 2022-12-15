09b4ab5339127ae47a599865545662fe57c8f559
kirankumargs18 <58082996+kirankumargs18@users.noreply.github.com>
2022-12-15 17:22:21 +0530

    Add files via upload

---
+++ spring_jpa_one_to_many_task/src/main/java/com/globallogic/SpringJpaOneToManyTaskApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOneToManyTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaOneToManyTaskApplication.class, args);
+	}
+
+}
